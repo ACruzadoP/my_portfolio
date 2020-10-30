5cb4358f7b25aa767aaffe03338b03d03dd17d71
Alberto <albertocruzadopleite@gmail.com>
2020-10-30 14:05:04 +0100

    Added info to About

--- src/components/Main/About.js
+++ src/components/Main/About.js
@@ -5,7 +5,14 @@ class About extends React.Component {
         return (
             <React.Fragment>
                 <div>
-                    ABOUT
+                    <p className="about">
+                        Hello there! My name is Alberto and technically, I am a Technician in Computer Applications Development. The reason why I said "technicaly" is that I have never considered myself a true developer - mainly because I have never officially worked as such but also, because every time I have talked to a profesional, I have realized how little I know. 
+                        <br></br><br></br>
+                        I have had all sort of jobs to be honest, but never as a developer. I enjoy every time I manage to develop a piece of software, so far with the only purpose of having fun, but that's all.
+                        <br></br><br></br>
+                        With that said, feel free to check out my little (but not insignificant) works. I trully hope you enjoy 'em all! =)
+                        <br></br><br></br><br></br><br></br>
+                    </p>
                 </div>
             </React.Fragment>
         )

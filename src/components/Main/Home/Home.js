5b1481a00bffb9d6b1dfec1b02c2451beec12d47
Alberto <albertocruzadopleite@gmail.com>
2020-11-08 17:02:14 +0100

    testing implementation for smartphone

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -3,6 +3,7 @@ import "./Home.css"
 import '../../Header/Navbar.css'
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
+var height = window.height();
 var carouselContainer;
 var carouselSlide;
 var carouselImages;
@@ -24,24 +25,13 @@ function nextBtn() {
 
 
 const resizeListener = async () => {
-    carouselContainer.style.opacity = 0;
-    await delay(1500);
-    window.location.reload();
-    
+    if ((window).height() === height){
+        carouselContainer.style.opacity = 0;
+        await delay(1500);
+        window.location.reload();
+    }
 };
-/*
-const scrollListener = async () => {
-    console.log("scroll!");
-    carouselContainer.style.opacity = 0;
-    await delay(1500);
-    window.location.reload();
-    
-};
-*/
 
-function scrollListener () {
-    console.log("scroll!");
-}
 
 
 class Home extends React.Component {
@@ -65,7 +55,6 @@ class Home extends React.Component {
             }
         });
         window.addEventListener("resize", resizeListener);
-        window.addEventListener("scroll", scrollListener);
         linkHome = document.getElementsByClassName("linkhome");
         linkHome[0].classList.toggle("toggle_home");
 
@@ -79,7 +68,6 @@ class Home extends React.Component {
     componentWillUnmount() {
         
         window.removeEventListener("resize", resizeListener);
-        window.removeEventListener("scroll", scrollListener);
         linkHome[0].classList.toggle("toggle_home");
 
     }
